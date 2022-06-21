f5acddd3933c024bd6fa0f1d4f0c1e1fd848c6c7
varshini <varshini.dinakaran@capgemini.com>
2022-06-21 15:04:27 +0530

    filehandling added

---
+++ FileHandling/main.py
@@ -0,0 +1,46 @@
+"""file operation can be done in following ways:
+1.open
+2.read/write
+3.close
+
+open
+------------
+open function accepts two arguments .file name and access mode
+it returns the file object
+syntax-file obj=open(<filename>,<access mode>)
+
+access mode
+--------------------
+r-opens the file in read only mode.pointer exists at the beginning.
+The file is by default open in this mode if no access mode is passed.
+
+w-write mode.
+if the file is not exist it creates one
+pointer exists at the beginning
+
+a-append mode
+It opens the file in the append mode. 
+The file pointer exists at the end of the previously written file if exists any.
+It creates a new file if no file exists with the same name.
+
+r+-It opens the file to read and write both. The file pointer exists at the beginning of the file.
+
+w+-It opens the file to write and read both. 
+It is different from r+ in the sense that it overwrites the previous file if one exists whereas r+ doesn't overwrite the previously written file. 
+It creates a new file if no file exists.
+The file pointer exists at the beginning of the file.
+
+a+-It opens a file to append and read both.
+ The file pointer remains at the end of the file if a file exists.
+ It creates a new file if no file exists with the same name.
+ 
+rb-It opens the file to read-only in binary format.
+The file pointer exists at the beginning of the file.
+
+wb-It opens the file to write only in binary format.
+It overwrites the file if it exists previously or creates a new one if no file exists.
+The file pointer exists at the beginning of the file.
+
+with stmt used to close the files automatically when it done
+
+"""""
---
+++ FileHandling/open.py
@@ -0,0 +1,14 @@
+# try:
+#     file_object=open("file.txt")
+#     if file_object:
+#         print("file opened")
+# finally:
+#     file_object.close()
+#
+with open("file.txt") as f:
+    content=f.read()
+    print(content)
+# fw=open("file.txt","a")
+# fw.write(" Python has an easy syntax and user-friendly interaction.")
+# fw.close()
+
