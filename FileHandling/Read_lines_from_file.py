40f2d63ec8f979a2770c24039033f7558cb36d88
varshini <varshini.dinakaran@capgemini.com>
2022-06-21 18:31:07 +0530

    Resolved conflicts and merging

--- FileHandling/Read_lines_from_file.py
+++ FileHandling/Read_lines_from_file.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 file1=open("file.txt")
 # content=file1.readline()
 # content2=file1.readline()
@@ -6,13 +5,4 @@ file1=open("file.txt")
 # print(content2)
 cont3=file1.readlines()
 print(cont3)
-=======
-file1=open("file.txt")
-# content=file1.readline()
-# content2=file1.readline()
-# print(content)
-# print(content2)
-cont3=file1.readlines()
-print(cont3)
->>>>>>> 03631c14cfc32b91e5704fdb44bb1c72b4f75522
 file1.close()
